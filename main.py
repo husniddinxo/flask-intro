2f1cc849bacc658387938ef204e87213ea0fe02d
husniddin <baxodirovhusniddin66@gemal.com>
2023-02-08 16:49:47 +0500

    ugbdijgorf

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello():
    return "<h1>Hello World!</h1>"
 
+
+@app.route("/about")
+def about ():
+   return "<h1>about page!</h1>"
+
+
 if __name__ == "__main__":
    app.run()
